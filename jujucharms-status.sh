510cb635d0cccafc5027818230f35a2e127e7c32
Brad Crittenden <bac@canonical.com>
2017-01-17 08:45:26 -0400

    Add remove-apps.py to remove all apps while keeping the controller and model.

--- jujucharms-status.sh
+++ jujucharms-status.sh
@@ -7,22 +7,23 @@
 set -eux
 protocol="https"
 prefix=""
+api="api"
 if [[ -z "$@" ]]; then
     domain="jujucharms.com"
 elif [[ $1 == "staging" ]]; then
     domain="staging.jujucharms.com"
-elif [[ $1 == "guimaas" ]]; then
+elif [[ $1 == "jujugui.org" ]]; then
     domain="jujugui.org"
     prefix="www."
+    api="www"
 else
     exit
 fi
    
 http $protocol://$prefix$domain/_version
-http $protocol://api.$domain/identity/debug/info
-http $protocol://api.$domain/identity/debug/status
-http $protocol://api.$domain/jem/debug/info
-http $protocol://api.$domain/jem/debug/status
-http $protocol://demo.$domain/version
-http $protocol://api.$domain/charmstore/debug/info
-http $protocol://api.$domain/charmstore/v5/debug/status
+http $protocol://$api.$domain/identity/debug/info
+http $protocol://$api.$domain/identity/debug/status
+http $protocol://$api.$domain/bundleservice/debug/info
+http $protocol://demo.$domain/static/gui/build/app/version.json
+http $protocol://$api.$domain/charmstore/debug/info
+http $protocol://$api.$domain/charmstore/v5/debug/status
